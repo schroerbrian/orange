be6e6e2eff51b093a05760fb2208535e9c437c1a
Brian Schroer <schroer.brian@gmail.com>
2014-02-17 16:51:36 -0800

    Test

--- homepage.rb
+++ homepage.rb
@@ -3,21 +3,21 @@ require 'sinatra'
 require 'httparty'
 
 # functions
-def get_park
-  current_park = nil
-  parks = HTTParty.get("http://mhpmproperties.herokuapp.com/api/parks")
+# def get_park
+#   current_park = nil
+#   parks = HTTParty.get("http://mhpmproperties.herokuapp.com/api/parks")
   
-  parks.each_with_index do |park, index|
-    if park["name"].split(" ").first.downcase == File.basename(Dir.getwd).downcase
-      current_park = parks[index]
-    end
-  end
-  return current_park
-end 
+#   parks.each_with_index do |park, index|
+#     if park["name"].split(" ").first.downcase == File.basename(Dir.getwd).downcase
+#       current_park = parks[index]
+#     end
+#   end
+#   return current_park
+# end 
 
 # routes
 get '/' do
-  current_park = get_park
+  current_park = "Orange Village"
   erb :index, locals: { current_park: current_park } 
 end
 
