05d1115c62ae5bda91526fa361d8f9c7a5c87cea
Brian Schroer <schroer.brian@gmail.com>
2014-02-17 16:53:26 -0800

    test two

--- homepage.rb
+++ homepage.rb
@@ -17,7 +17,15 @@ require 'httparty'
 
 # routes
 get '/' do
-  current_park = "Orange Village"
+  current_park = nil
+  parks = HTTParty.get("http://mhpmproperties.herokuapp.com/api/parks")
+  
+  parks.each_with_index do |park, index|
+    if park["name"].split(" ").first.downcase == File.basename(Dir.getwd).downcase
+      current_park = parks[index]
+    end
+  end
+
   erb :index, locals: { current_park: current_park } 
 end
 
