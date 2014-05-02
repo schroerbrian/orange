f744e24656b1837b5ad04768fd1c129971ebca09
Brian Schroer <schroer.brian@gmail.com>
2014-05-02 15:29:02 -0700

    Add phone and email info. Change to contact sidebar and footer to partials.

--- homepage.rb
+++ homepage.rb
@@ -4,67 +4,79 @@ require 'httparty'
 require 'sass'
 require 'newrelic_rpm'
 
-# functions
-def get_park
-  current_park = nil
-  parks = HTTParty.get("http://mhpmproperties.herokuapp.com/api/parks") 
-  puts parks 
-  parks.each_with_index do |park, index|
-    if park["name"].split(" ").first.downcase == "orange"
-      current_park = parks[index]
-      puts current_park["name"] + " is the current_park"
-    else 
-      puts "no current park"
+class WebApp < Sinatra::Base
+
+  # functions
+  def get_park
+    current_park = nil
+    parks = HTTParty.get("http://mhpmproperties.herokuapp.com/api/parks") 
+    puts parks 
+    parks.each_with_index do |park, index|
+      if park["name"].split(" ").first.downcase == "orange"
+        current_park = parks[index]
+        puts current_park["name"] + " is the current_park"
+      else 
+        puts "no current park"
+      end
     end
+    return current_park
   end
-  return current_park
-end
 
-def get_homes
-  orange_id = 1
-  homes = HTTParty.get("http://mhpmproperties.herokuapp.com/api/homes")
-  puts homes
-  homes.each {|h|
-    if h['park_id'] != orange_id
-      homes.delete(h)
-    end
-  }
-  return homes 
-end
+  def get_homes
+    orange_id = 1
+    homes = HTTParty.get("http://mhpmproperties.herokuapp.com/api/homes")
+    puts homes
+    homes.each {|h|
+      if h['park_id'] != orange_id
+        homes.delete(h)
+      end
+    }
+    return homes 
+  end
 
-# routes
-get '/' do
-  current_park = get_park
-  erb :index, locals: { current_park: current_park } 
-end
+  before do
+    park = get_park
+    @phone = park["phone_number"]
+  end
 
-get '/homes' do
-  homes = get_homes
-  erb :homes, locals: { homes: homes } 
-end 
+  # routes
+  get '/' do
+    current_park = get_park
+    erb :index, locals: { current_park: current_park } 
+  end
 
-get '/gallery' do
-  current_park = get_park
-  erb :gallery, locals: { current_park: current_park } 
-end 
+  get '/homes' do
+    homes = get_homes
+    erb :homes, locals: { homes: homes } 
+  end 
 
-get '/contact' do
-  current_park = get_park
-  erb :contact, locals: { current_park: current_park } 
-end 
+  get '/gallery' do
+    current_park = get_park
+    erb :gallery, locals: { current_park: current_park } 
+  end 
 
-get "/main.css" do
-  scss :main
-end
+  get '/contact' do
+    current_park = get_park
+    erb :contact, locals: { current_park: current_park } 
+  end 
 
-get "/homes.css" do
-  scss :homes
-end
+  get "/main.css" do
+    scss :main
+  end
 
-get "/normalize.css" do
-  scss :normalize
-end
+  get "/homes.css" do
+    scss :homes
+  end
+
+  get "/normalize.css" do
+    scss :normalize
+  end
 
 # get '/*' do
 #   redirect('/')
 # end
+
+end
+
+WebApp.run!
+
